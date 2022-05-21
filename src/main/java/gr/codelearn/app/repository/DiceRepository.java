5ad34484c884b6d8e0a298c067d8fbe1a062dc8a
Rozakos <billmewtwo1996@gmail.com>
2022-05-21 19:23:49 +0300

    Added geometric shape tracker

--- src/main/java/gr/codelearn/app/repository/DiceRepository.java
+++ src/main/java/gr/codelearn/app/repository/DiceRepository.java
@@ -100,6 +100,38 @@ public class DiceRepository {
         } catch (SQLException e) {
             log.error("For some reason, a connection could not be obtained", e);
         }
+    }// Geometric Shape Tracker
+
+    public List<Die> getAllGeometricShapeResults() {
+        List<Die> allGeometricShapeResults = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM GEOMETRICSHAPETRACKER";
+            Connection connection = DataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                long id = resultSet.getLong(1);
+                int result = resultSet.getInt(2);
+                Timestamp throwDate = resultSet.getTimestamp(3);
+                allGeometricShapeResults.add(new Die(id, result, throwDate));
+            }
+        } catch (SQLException e) {
+            log.error("For some reason, a connection could not be obtained", e);
+        }
+        return allGeometricShapeResults;
     }
 
+    public void saveGeometricShape(int result) {
+        try {
+            String query = "INSERT INTO GEOMETRICSHAPETRACKER(result, throw_date) VALUES(?, ?)";
+            Connection connection = DataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, result);
+            // created a date (current date) and sets it as SQL's timestamp instance, which is required
+            preparedStatement.setTimestamp(2, new Timestamp(new Date().getTime()));
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            log.error("For some reason, a connection could not be obtained", e);
+        }
+    }
 }
